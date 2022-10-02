dafef6dcd05475b7dad11803441eb289022a4c07
ravishyadavcodes <114696127+ravishyadavcodes@users.noreply.github.com>
2022-10-02 15:48:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,36 @@ function unique(board, index, value) {
     }
     return true;
 }
+function one_value_cell_constraint(board) {
+  updated = false
+  // Convert every gap into an array of possibilities
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      if (board[r][c] == 0) {
+        updated = complete_cell(board, r, c) || updated
+      }
+    }
+  }
+  // Look out for any possibility that appears as a possibility
+  // once-only in the row, column, or quadrant.
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      if (Array.isArray(board[r][c])) {
+        let possibilities = board[r][c]
+        updated = 
+          appears_once_only(board, possibilities, get_row(board, r), r, c) ||
+          appears_once_only(board, possibilities, get_column(board, c), r, c) ||
+          appears_once_only(board, possibilities, get_square(board, square_coordinates[r][c]), r, c) || updated
+      }
+    }
+  }
+  // Reinitialize gaps back to zero before ending
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      if (Array.isArray(board[r][c])) {
+        board[r][c] = 0
+      }
+    }
+  }
+  return updated
+}
